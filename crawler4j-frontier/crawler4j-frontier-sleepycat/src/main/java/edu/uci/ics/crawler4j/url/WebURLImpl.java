df6cd92d3216124862f36fc094b8d5639d640a79
rzo1 <13417392+rzo1@users.noreply.github.com>
2020-12-19 16:03:01 +0100

    re-adds apache2 license

--- crawler4j-frontier/crawler4j-frontier-sleepycat/src/main/java/edu/uci/ics/crawler4j/url/WebURLImpl.java
+++ crawler4j-frontier/crawler4j-frontier-sleepycat/src/main/java/edu/uci/ics/crawler4j/url/WebURLImpl.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * de.hs-heilbronn.mi:crawler4j-frontier-sleepycat
+ * %%
+ * Copyright (C) 2010 - 2020 crawler4j-fork (pre-fork: Yasser Ganjisaffar)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package edu.uci.ics.crawler4j.url;
 
 import java.util.Map;
